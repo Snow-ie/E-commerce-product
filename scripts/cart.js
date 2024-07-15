bf0891b3eef4f42f579e12ee8906417cac72bc56
Ekeh Pearl <pearlekeh7@gmail>
2024-07-15 10:08:25 +0100

    first commit

---
+++ scripts/cart.js
@@ -0,0 +1,123 @@
+"use strict";
+
+const cartDecrement = document.querySelector(".addtocart > :first-child");
+const itemNo = document.querySelector(".addtocart > :nth-child(2)");
+const cartIncremeter = document.querySelector(".addtocart > :last-child");
+
+let indexNo = 0;
+
+cartIncremeter.addEventListener("click", () => {
+  itemNo.textContent = indexNo++;
+  if (indexNo > 10) indexNo = 10;
+  itemNo.textContent = indexNo;
+  itemNo.style.color = "#000";
+});
+
+cartDecrement.addEventListener("click", () => {
+  itemNo.textContent = indexNo--;
+  if (indexNo < 0) indexNo = 0;
+  itemNo.textContent = indexNo;
+});
+
+const cartIcon = document.querySelector(".cartBtn");
+const cart = document.querySelector(".cart");
+
+cartIcon.addEventListener("click", (e) => {
+  cart.classList.toggle("hide");
+  e.stopPropagation();
+});
+
+// dynamically adding contents to the cart
+const button = document.getElementById("button");
+const notification = document.querySelector(".notification");
+const message = document.querySelectorAll(".message");
+const item = document.querySelector(".addtocart > :nth-child(2)");
+
+button.addEventListener("click", () => {
+  if (item.textContent > 0) {
+    notification.style.visibility = "visible";
+
+    // message displayed on successfully adding items to cart
+    message[0].classList.add("render");
+    setTimeout(() => {
+      message[0].classList.remove("render");
+    }, 3000);
+
+    cart.innerHTML = `<div class="name">Cart</div>
+        <div class="addedCartContainer">
+          <figcaption class="thumb">
+            <img src="./images/image-product-1.jpg" alt="image product 1" />
+          </figcaption>
+
+          <div class="cartDetail">
+            <p class="detailName">Fall Limited Edition sneakers</p>
+            <p class="detailPrice">$125.00 x <span>3</span> <b>$375.00</b></p>
+          </div>
+
+          <figcaption class="trash">
+            <img src="./images/icon-delete.svg" alt="icon delete" />
+          </figcaption>
+        </div>
+
+        <button id="checkout">Checkout</button>`;
+
+    const spanEl = document.querySelector("span");
+    const total = document.querySelector("b");
+
+    spanEl.textContent = item.textContent;
+    let result = 125.0 * Number(spanEl.textContent);
+    total.textContent = `$${result}.00`;
+
+    trashCan();
+    checkOutFunc();
+  } else {
+    // error message to be displayed if there's no item number specified to be added
+    message[1].classList.add("renderMsg");
+    setTimeout(() => {
+      message[1].classList.remove("renderMsg");
+    }, 3000);
+  }
+});
+
+const trashCan = function () {
+  const trash = document.querySelector(".trash");
+
+  trash.addEventListener("click", () => {
+    cart.innerHTML = `<div class="name">Cart</div>`;
+    notification.style.visibility = "hidden";
+
+    // message displayed on successfully deleting items from the cart
+    message[2].classList.add("show");
+    setTimeout(() => {
+      message[2].classList.remove("show");
+    }, 3000);
+
+    cart.classList.add("hide");
+  });
+};
+
+const checkOutFunc = function () {
+  const checkout = document.getElementById("checkout");
+
+  checkout.addEventListener("click", () => {
+    console.log("I'm being clicked");
+    cart.innerHTML = `
+                  <div class="name">Cart</div>
+                  <figcaption class="verified">
+                          <img src="./design/verified.gif" alt="an image">
+                      </figcaption>`;
+
+    notification.style.visibility = "hidden";
+
+    setTimeout(() => {
+      cart.innerHTML = `<div class="name">Cart</div>`;
+      cart.classList.add("hide");
+    }, 2000);
+  });
+};
+
+document.addEventListener("click", () => {
+  if (!cart.classList.contains("hide")) {
+    cart.classList.toggle("hide");
+  }
+});
---
+++ scripts/modal.js
@@ -0,0 +1,85 @@
+"use strict";
+
+const main = document.querySelector("main");
+const modal = document.querySelector(".modal");
+
+rendered_image.addEventListener("click", (e) => {
+  main.style.background = "linear-gradient(45deg, black, transparent)";
+  main.style.opacity = "20%";
+  main.style.filter = "blur(5px)";
+
+  modal.classList.remove("hide");
+  e.stopPropagation();
+});
+
+const rendered_image_modal = document.querySelector(".modalRendered img");
+const modal_thumbnails = document.querySelectorAll(".modal_thumbnails img");
+
+modal_thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", () => {
+    (function () {
+      modal_thumbnails.forEach((thumb) => {
+        if (thumb.classList.contains("active"))
+          thumb.classList.remove("active");
+      });
+    })();
+
+    rendered_image_modal.src = thumbnail.src;
+    thumbnail.classList.add("active");
+  });
+});
+
+const closeModal = document.querySelector('img[src="./images/icon-close.svg"]');
+
+closeModal.addEventListener("click", () => {
+  main.style.background = "#fff";
+  main.style.opacity = "100%";
+  main.style.filter = "blur(0px)";
+
+  document.querySelector(".modal").classList.add("hide");
+});
+
+const modalarrow = document.querySelectorAll(".modalarrow");
+
+let modalIndex = 0;
+
+modalarrow[1].addEventListener("click", () => {
+  modalIndex++;
+
+  if (modalIndex > images.length - 1) modalIndex = 0;
+
+  rendered_image_modal.src = images[modalIndex];
+  rendered_image_modal.alt = alt[modalIndex];
+  iterator();
+});
+
+modalarrow[0].addEventListener("click", () => {
+  modalIndex--;
+
+  if (modalIndex < 0) modalIndex = images.length - 1;
+
+  rendered_image_modal.src = images[modalIndex];
+  rendered_image_modal.alt = alt[modalIndex];
+  iterator();
+});
+
+const iterator = function () {
+  (function () {
+    modal_thumbnails.forEach((thumb) => {
+      if (thumb.classList.contains("active")) thumb.classList.remove("active");
+    });
+  })();
+  console.log(modalIndex);
+
+  modal_thumbnails.forEach((thumb) => {
+    modal_thumbnails[modalIndex].classList.add("active");
+  });
+};
+
+main.addEventListener("click", (e) => {
+  main.style.background = "#fff";
+  main.style.opacity = "100%";
+  main.style.filter = "blur(0px)";
+
+  document.querySelector(".modal").classList.add("hide");
+});
